d1156547adbb0d6538ca64f007deb73f6e7ed2e5
singyuKang <rkdtlscks123@naver.com>
2024-11-06 17:41:35 +0900

    chore : 상수값 추가

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+export const ERROR_MESSAGE = Object.freeze({
+  READ_ITEM: "[ERROR] : 구매하실 상품명과 수량을 형식에 맞춰 입력해주세요",
+  FILE_PARSING: "[ERROR] 잘못된 파일 형식 입니다.",
+});
+
+export const INPUT_MESSAGE = Object.freeze({
+  READ_ITEM:
+    "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])",
+});
+
+export const OUTPUT_MESSAGE = Object.freeze({
+  MAIN_TITLE: "안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.",
+});
+
+export const PRODUCT_FILE_PATH = "./public/products.md";
+export const PROMOTION_FILE_PATH = "./public/promotions.md";
