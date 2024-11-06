4239b087a18b5fd6eaccd10f8060ffbda27810cc
singyuKang <rkdtlscks123@naver.com>
2024-11-06 17:08:51 +0900

    feat : FileManager 파일 입출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { PRODUCT_FILE_PATH, PROMOTION_FILE_PATH } from "./constants.js";
+import { FileManager } from "./utils/fileManager.js";
+
 class App {
-  async run() {}
+  async run() {
+    const fileManager = new FileManager();
+    const productsParsing = await fileManager.parseFile(PRODUCT_FILE_PATH);
+    const promotionsParsing = await fileManager.parseFile(PROMOTION_FILE_PATH);
+  }
 }
 
 export default App;
