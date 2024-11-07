07b8812abbeb28e580d0c622dcb6cf082d4564f9
singyuKang <rkdtlscks123@naver.com>
2024-11-07 18:01:11 +0900

    chore : 상수값 추가

--- src/constants.js
+++ src/constants.js
@@ -1,15 +1,26 @@
+import { productInfoOutputString } from "./utils/stringHandler.js";
+
 export const ERROR_MESSAGE = Object.freeze({
   READ_ITEM: "[ERROR] : 구매하실 상품명과 수량을 형식에 맞춰 입력해주세요",
+  OVER_QUANTITY:
+    "[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.",
+  NOT_IN_PRODUCTS: "[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.",
   FILE_PARSING: "[ERROR] 잘못된 파일 형식 입니다.",
 });
 
 export const INPUT_MESSAGE = Object.freeze({
   READ_ITEM:
-    "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])",
+    "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n",
 });
 
 export const OUTPUT_MESSAGE = Object.freeze({
-  MAIN_TITLE: "안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.",
+  MAIN_TITLE: "안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n",
+  PRODUCT_INFO: (product) =>
+    `- ${product.name} ${
+      product.price
+    } ${productInfoOutputString.quantityString(
+      product.quantity
+    )} ${productInfoOutputString.promotionString(product.promotion)}`,
 });
 
 export const PRODUCT_FILE_PATH = "./public/products.md";
