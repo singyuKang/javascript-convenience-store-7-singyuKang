37daac52fe5a7d06b4b4b7389d9da83e9487ff2b
singyuKang <rkdtlscks123@naver.com>
2024-11-10 18:40:14 +0900

    chore : 로컬 테스트

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,77 +1,60 @@
 import App from '../src/App.js';
 import { MissionUtils } from '@woowacourse/mission-utils';
 import { EOL as LINE_SEPARATOR } from 'os';
-
 const mockQuestions = (inputs) => {
   const messages = [];
-
   MissionUtils.Console.readLineAsync = jest.fn((prompt) => {
     messages.push(prompt);
     const input = inputs.shift();
-
     if (input === undefined) {
       throw new Error('NO INPUT');
     }
-
     return Promise.resolve(input);
   });
-
   MissionUtils.Console.readLineAsync.messages = messages;
 };
-
 const mockNowDate = (date = null) => {
   const mockDateTimes = jest.spyOn(MissionUtils.DateTimes, 'now');
   mockDateTimes.mockReturnValue(new Date(date));
   return mockDateTimes;
 };
-
 const getLogSpy = () => {
   const logSpy = jest.spyOn(MissionUtils.Console, 'print');
   logSpy.mockClear();
   return logSpy;
 };
-
 const getOutput = (logSpy) => {
   return [...logSpy.mock.calls].join(LINE_SEPARATOR);
 };
-
 const expectLogContains = (received, expects) => {
   expects.forEach((exp) => {
     expect(received).toContain(exp);
   });
 };
-
 const expectLogContainsWithoutSpacesAndEquals = (received, expects) => {
   const processedReceived = received.replace(/[\s=]/g, '');
   expects.forEach((exp) => {
     expect(processedReceived).toContain(exp);
   });
 };
-
 const runExceptions = async ({ inputs = [], inputsToTerminate = [], expectedErrorMessage = '' }) => {
   // given
   const logSpy = getLogSpy();
   mockQuestions([...inputs, ...inputsToTerminate]);
-
   // when
   const app = new App();
   await app.run();
-
   // then
   expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(expectedErrorMessage));
 };
-
 const run = async ({ inputs = [], inputsToTerminate = [], expected = [], expectedIgnoringWhiteSpaces = [] }) => {
   // given
   const logSpy = getLogSpy();
   mockQuestions([...inputs, ...inputsToTerminate]);
-
   // when
   const app = new App();
   await app.run();
-
   const output = getOutput(logSpy);
-
   // then
   if (expectedIgnoringWhiteSpaces.length > 0) {
     expectLogContainsWithoutSpacesAndEquals(output, expectedIgnoringWhiteSpaces);
@@ -80,15 +63,12 @@ const run = async ({ inputs = [], inputsToTerminate = [], expected = [], expecte
     expectLogContains(output, expected);
   }
 };
-
 const INPUTS_TO_TERMINATE = ['[비타민워터-1]', 'N', 'N'];
-
 describe('편의점', () => {
   afterEach(() => {
     jest.clearAllMocks();
     jest.restoreAllMocks();
   });
-
   test('파일에 있는 상품 목록 출력', async () => {
     await run({
       inputs: ['[콜라-1]', 'N', 'N'],
@@ -115,23 +95,19 @@ describe('편의점', () => {
       ],
     });
   });
-
   test('여러 개의 일반 상품 구매', async () => {
     await run({
       inputs: ['[비타민워터-3],[물-2],[정식도시락-2]', 'N', 'N'],
       expectedIgnoringWhiteSpaces: ['내실돈18,300'],
     });
   });
-
   test('기간에 해당하지 않는 프로모션 적용', async () => {
     mockNowDate('2024-02-01');
-
     await run({
       inputs: ['[감자칩-2]', 'N', 'N'],
       expectedIgnoringWhiteSpaces: ['내실돈3,000'],
     });
   });
-
   test('예외 테스트', async () => {
     await runExceptions({
       inputs: ['[컵라면-12]', 'N', 'N'],
