63246b68162949446575bc8b5996121ceef91d60
singyuKang <rkdtlscks123@naver.com>
2024-11-10 20:43:41 +0900

    test : App.js 테스트 코드 연결

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import FileManager from './utils/FileManager.js';
+import ConvenienceController from './controller/ConvenienceController.js';
+import Products from './domain/Products.js';
+import Promotions from './domain/Promotions.js';
+import { InputView } from './view/InputView.js';
+import OutputView from './view/OutputView.js';
+import { PRODUCT_FILE_PATH, PROMOTION_FILE_PATH, USER_SAY_YES } from './constants.js';
 
 class App {
   async run() {
     // // 파일 입출력
     const fileManager = new FileManager();
-    // const productsParsing = await fileManager.parseFile(PRODUCT_FILE_PATH);
-    // const promotionsParsing = await fileManager.parseFile(PROMOTION_FILE_PATH);
-    // // 도메인 변환
-    // const products = new Products(productsParsing);
-    // const promotions = new Promotions(promotionsParsing);
-    // // 편의점 컨트롤러 생성
-    // const convenienceController = new ConvenienceController(products, promotions);
-    // let additionalPurchase;
-    // do {
-    //   OutputView.printMainTitle();
-    //   OutputView.printProductsInfo(convenienceController.products);
-    //   const readItems = await InputView.readItem(convenienceController.products);
-    //   await Promise.all(readItems.map((readItem) => convenienceController.calculateUserProducts(readItem)));
-    //   const membership = await InputView.memberShip();
-    //   OutputView.printReceipt(convenienceController.convenienceResultController, membership);
-    //   additionalPurchase = await InputView.additionalPurchase();
-    //   if (additionalPurchase === USER_SAY_YES) {
-    //     convenienceController.convenienceResultController.resetProducts();
-    //   }
-    // } while (additionalPurchase === USER_SAY_YES);
+    const productsParsing = await fileManager.parseFile(PRODUCT_FILE_PATH);
+    const promotionsParsing = await fileManager.parseFile(PROMOTION_FILE_PATH);
+    // 도메인 변환
+    const products = new Products(productsParsing);
+    const promotions = new Promotions(promotionsParsing);
+    // 편의점 컨트롤러 생성
+    const convenienceController = new ConvenienceController(products, promotions);
+    let additionalPurchase;
+    do {
+      OutputView.printMainTitle();
+      OutputView.printProductsInfo(convenienceController.products);
+      const readItems = await InputView.readItem(convenienceController.products);
+      await Promise.all(readItems.map((readItem) => convenienceController.calculateUserProducts(readItem)));
+      const membership = await InputView.memberShip();
+      OutputView.printReceipt(convenienceController.convenienceResultController, membership);
+      additionalPurchase = await InputView.additionalPurchase();
+      if (additionalPurchase === USER_SAY_YES) {
+        convenienceController.convenienceResultController.resetProducts();
+      }
+    } while (additionalPurchase === USER_SAY_YES);
   }
 
   async startConvenienceStore(convenienceController) {}
