486f2882cc83ce64445378d4c2a3223d39bb3e9b
singyuKang <rkdtlscks123@naver.com>
2024-11-10 17:46:58 +0900

    chore : 주석 되돌리기

--- src/App.js
+++ src/App.js
@@ -10,30 +10,35 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async run() {
     // 파일 입출력
-    // const fileManager = new FileManager();
-    // const productsParsing = await fileManager.parseFile(PRODUCT_FILE_PATH);
-    // const promotionsParsing = await fileManager.parseFile(PROMOTION_FILE_PATH);
-    // // 도메인 변환
-    // const products = new Products(productsParsing);
-    // const promotions = new Promotions(promotionsParsing);
-    // // 편의점 컨트롤러 생성
-    // const convenienceController = new ConvenienceController(products, promotions);
-    // OutputView.printMainTitle();
-    // OutputView.printProductsInfo(convenienceController.products);
-    // // 구매할 상품 수량입력
-    // const readItems = await InputView.readItem(convenienceController.products);
-    // // 계산처리
-    // await Promise.all(readItems.map((readItem) => convenienceController.calculateUserProducts(readItem)));
-    // // 멤버십 입력 확인
-    // const membership = await InputView.memberShip();
-    // // 구매 상품 내역 증정, 상품 내역, 금액 정보를 출력
-    // OutputView.printReceipt(convenienceController.convenienceResultController, membership);
-    // // 추가 구매 여부 확인 안내 문구 여기서 시작부터 반복할수있도록
-    // const additionalPurchase = await InputView.additionalPurchase();
-    // if (additionalPurchase === 'Y') {
-    //   convenienceController.convenienceResultController.resetProducts();
-    //   this.startConvenienceStore(convenienceController);
-    // }
+    const fileManager = new FileManager();
+    const productsParsing = await fileManager.parseFile(PRODUCT_FILE_PATH);
+    const promotionsParsing = await fileManager.parseFile(PROMOTION_FILE_PATH);
+
+    // 도메인 변환
+    const products = new Products(productsParsing);
+    const promotions = new Promotions(promotionsParsing);
+
+    // 편의점 컨트롤러 생성
+    const convenienceController = new ConvenienceController(products, promotions);
+
+    OutputView.printMainTitle();
+    OutputView.printProductsInfo(convenienceController.products);
+    // 구매할 상품 수량입력
+    const readItems = await InputView.readItem(convenienceController.products);
+
+    // 계산처리
+    await Promise.all(readItems.map((readItem) => convenienceController.calculateUserProducts(readItem)));
+    // 멤버십 입력 확인
+    const membership = await InputView.memberShip();
+    // 구매 상품 내역 증정, 상품 내역, 금액 정보를 출력
+    OutputView.printReceipt(convenienceController.convenienceResultController, membership);
+    // 추가 구매 여부 확인 안내 문구 여기서 시작부터 반복할수있도록
+    const additionalPurchase = await InputView.additionalPurchase();
+    if (additionalPurchase === 'Y') {
+      convenienceController.convenienceResultController.resetProducts();
+      this.startConvenienceStore(convenienceController);
+    }
+
     // 시작
     // this.startConvenienceStore(convenienceController);
   }
