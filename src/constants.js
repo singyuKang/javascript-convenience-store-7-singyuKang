b2cde2628d7c0cb3340b70e1595eeb269b6a91ea
singyuKang <rkdtlscks123@naver.com>
2024-11-11 22:17:07 +0900

    chore : READ_ITEM 에러 문구 수정

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 import { comma, productInfoOutputString } from './utils/stringHandler.js';
 
 export const ERROR_MESSAGE = Object.freeze({
-  READ_ITEM: '[ERROR] : 구매하실 상품명과 수량을 형식에 맞춰 입력해주세요',
+  READ_ITEM: '[ERROR] : 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요',
   OVER_QUANTITY: '[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.',
   NOT_IN_PRODUCTS: '[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.',
   FILE_PARSING: '[ERROR] 잘못된 파일 형식 입니다.',
